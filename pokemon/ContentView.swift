08c677c310f980715bc3c22cae5ace1ae95c97ee
tinashe <tinashe@whereismytransport.com>
2022-07-21 13:22:51 +0200

    Initial Commit

---
+++ pokemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokemon
+//
+//  Created by Tinashe Madzimbamuto on 2022/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by Tinashe Madzimbamuto on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
